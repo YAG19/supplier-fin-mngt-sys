95465a8c8703b77895f6c050500ce886d33cf2e9
[yagne] <[yagnesh.patel9898@gmail.com]>
2023-02-18 23:32:36 +0530

    controller

--- backend/src/main/java/com/capstoneproj/supfinsys/controller/ClientRegistrationController.java
+++ backend/src/main/java/com/capstoneproj/supfinsys/controller/ClientRegistrationController.java
@@ -4,7 +4,6 @@ import com.capstoneproj.supfinsys.models.Client;
 import com.capstoneproj.supfinsys.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
